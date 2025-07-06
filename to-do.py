9b8ccbba26ec1b9970cbb11c600f58fcd03b8a74
Rinchhen <trinchhen272@gmail.com>
2025-07-06 11:01:15 +0545

    new ability

--- to-do.py
+++ to-do.py
@@ -1,3 +1,5 @@
+import sys
+
 tasks = []
 
 def list_tasks():
@@ -8,5 +10,23 @@ def list_tasks():
         for i, task in enumerate(tasks, 1):
             print(f"{i}. {task}")
 
+def add_task(task):
+    tasks.append(task)
+    print(f"Added task: {task}")
+
 if __name__ == "__main__":
-    list_tasks()
+    if len(sys.argv) < 2:
+        print("Usage: python to-do.py [add/list] [task]")
+    else:
+        command = sys.argv[1]
+
+        if command == "list":
+            list_tasks()
+        elif command == "add":
+            if len(sys.argv) < 3:
+                print("Please provide a task to add.")
+            else:
+                task = " ".join(sys.argv[2:])
+                add_task(task)
+        else:
+            print("Unknown command. Use 'add' or 'list'.")
